9d439e1246558a492fef3bcdd748206419d1b08b
Godwin <godwinchuks032@gmail.com>
2024-06-25 23:58:56 +0100

    Basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -21,14 +21,12 @@ class BasicCache(BaseCaching):
     def __init__(self):
         """Initilizing Basic Cache"""
         super().__init__()
-        self.cache_data = {}
-        self.MAX_ITEMS = float('inf')
 
     def put(self, key, item):
         """Add an item in the cache. If the key or item is None, do nothing."""
-        if key is not None and item is not None:
-            self.cache_data[key] = item
-            self.print_cache()
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
 
     def get(self, key):
         """Get an item by key. If the key is None or doesn't exist,
