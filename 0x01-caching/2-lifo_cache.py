c91c88666ae04ad18980fe4e570d90ff6f18b095
Godwin <godwinchuks032@gmail.com>
2024-06-26 01:52:43 +0100

    LIFO Caching

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""LIFO Cache Module Three"""
+
+from base_caching import BaseCaching
+
+class LIFOCache(BaseCaching):
+    """class LIFOCache that inherits from BaseCaching and is a caching system:
+        You can overload def __init__(self): but don’t forget to call the parent init: super().__init__()
+        def put(self, key, item):
+    Args:
+        Must assign to the dictionary self.cache_data the item value for the key key.
+        If key or item is None, this method should not do anything.
+        If the number of items in self.cache_data is higher that BaseCaching.MAX_ITEMS:
+        you must discard the last item put in cache (LIFO algorithm)
+        you must print DISCARD: with the key discarded and following by a new line
+        def get(self, key):
+    Return:
+        the value in self.cache_data linked to key.
+        If key is None or if the key doesn’t exist in self.cache_data, return None.
+    """
+    def __init__(self):
+        """ Initialize the cache """
+        super().__init__()
+        self.cache_order = []
+    
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_order.remove(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            last_key = self.cache_order.pop()
+            del self.cache_data[last_key]
+            print("DISCARD:", last_key)
+
+        self.cache_data[key] = item   
+        self.cache_order.append(key)
+                
+    def get(self, key):
+        """ Get an item from the cache """
+        return self.cache_data(key, None)
