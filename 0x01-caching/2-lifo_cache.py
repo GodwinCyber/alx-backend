c750491fbcb85069bae521a5b023b86fb70660b9
Godwin <godwinchuks032@gmail.com>
2024-06-26 02:34:39 +0100

    Fifo caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -3,26 +3,27 @@
 
 from base_caching import BaseCaching
 
+
 class LIFOCache(BaseCaching):
     """class LIFOCache that inherits from BaseCaching and is a caching system:
-        You can overload def __init__(self): but don’t forget to call the parent init: super().__init__()
-        def put(self, key, item):
+        You can overload def __init__(self): but don’t forget to call the
+        parent init: super().__init__() def put(self, key, item):
     Args:
-        Must assign to the dictionary self.cache_data the item value for the key key.
-        If key or item is None, this method should not do anything.
-        If the number of items in self.cache_data is higher that BaseCaching.MAX_ITEMS:
-        you must discard the last item put in cache (LIFO algorithm)
-        you must print DISCARD: with the key discarded and following by a new line
-        def get(self, key):
+        Must assign to the dictionary self.cache_data the item value for the
+        key key. If key or item is None, this method should not do anything.
+        If the number of items in self.cache_data is higher that
+        BaseCaching.MAX_ITEMS: you must discard the last item put in cache
+        (LIFO algorithm) you must print DISCARD: with the key discarded and
+        following by a new line def get(self, key):
     Return:
-        the value in self.cache_data linked to key.
-        If key is None or if the key doesn’t exist in self.cache_data, return None.
+        the value in self.cache_data linked to key. If key is None or
+        if the key doesn’t exist in self.cache_data, return None.
     """
     def __init__(self):
         """ Initialize the cache """
         super().__init__()
         self.cache_order = []
-    
+
     def put(self, key, item):
         """ Add an item in the cache """
         if key is None or item is None:
@@ -35,9 +36,9 @@ class LIFOCache(BaseCaching):
             del self.cache_data[last_key]
             print("DISCARD:", last_key)
 
-        self.cache_data[key] = item   
+        self.cache_data[key] = item
         self.cache_order.append(key)
-                
+
     def get(self, key):
         """ Get an item from the cache """
         return self.cache_data(key, None)
