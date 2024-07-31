905cf4583a0b67b5de9ed963a634fb2bb60e9953
Godwin <godwinchuks032@gmail.com>
2024-07-31 09:20:26 +0100

    Node Redis client publisher and subscriber

--- 0x03-queuing_system_in_js/5-publisher.js
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -16,6 +16,10 @@ client.on('error', (err) => {
     console.log('Redis client not connected to the server:', err.toString());
 });
 
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
 const publishMessage = (message, time) => {
     setTimeout(() => {
         console.log(`About to send ${message}`);
@@ -23,11 +27,7 @@ const publishMessage = (message, time) => {
     }, time);
 };
 
-client.on('connect', () => {
-    console.log('Redis client connected to the server');
-});
-
-publishMessage('Holberton Student #1 starts course', 100);
-publishMessage('Holberton Student #2 starts course', 200);
-publishMessage('KILL_SERVER', 300);
-publishMessage('Holberton Student #3 starts course', 400);
+publishMessage("Holberton Student #1 starts course", 100);
+publishMessage("Holberton Student #2 starts course", 200);
+publishMessage("KILL_SERVER", 300);
+publishMessage("Holberton Student #3 starts course", 400);
